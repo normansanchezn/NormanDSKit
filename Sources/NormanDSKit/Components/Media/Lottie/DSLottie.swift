9bef352bbf38d9c3f83a99ca89f4b22af86db4a5
Norman Sanchez <normansanchezn@gmail.com>
2025-12-22 00:52:28 -0600

    [✨] Handle bundle on Lottie Component

--- Sources/NormanDSKit/Components/Media/Lottie/DSLottie.swift
+++ Sources/NormanDSKit/Components/Media/Lottie/DSLottie.swift
@@ -12,24 +12,24 @@ public struct DSLottie: View {
     private let animationName: String
     private let width: CGFloat?
     private let height: CGFloat?
-    
+    private let bundle: Bundle
+
     public init(
         animationName: String,
         width: CGFloat? = 420,
-        height: CGFloat? = 340
+        height: CGFloat? = 340,
+        bundle: Bundle = .main
     ) {
         self.animationName = animationName
         self.width = width
         self.height = height
+        self.bundle = bundle
     }
-    
+
     public var body: some View {
-        LottieView(animation: .named(animationName))
-            .playbackMode(
-                .playing(
-                    .toProgress(1, loopMode: .loop)
-                )
-            )
+        LottieView(animation: .named(animationName, bundle: bundle))
+            .playbackMode(.playing(.toProgress(1, loopMode: .loop)))
             .frame(width: width, height: height)
     }
 }
+
