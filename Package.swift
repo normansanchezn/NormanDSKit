d4d40eb02f5862bf2b3455ae65927e503f1eae79
Norman Sanchez <normansanchezn@gmail.com>
2025-12-23 18:36:28 -0600

    [🐛] Fix issue: // swift-tools-version: 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
         // Lottie (Airbnb)
         .package(
             url: "https://github.com/airbnb/lottie-spm.git",
-            from: "4.5.0"
+            from: "4.5.2"
         ),
         
         // LoremSwiftum (faker generator)
